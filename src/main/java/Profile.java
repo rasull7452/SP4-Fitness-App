5a7dc7429f34889f62e59f3fda82f0f36e9a3235
rasull7452 <rasullhossein250@gmail.com>
2025-12-09 14:10:27 +0100

    begynder på profile og user classes

---
+++ src/main/java/Profile.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Profile {
+    User user;
+    Map <String, Object> stats = new HashMap<>();
+    double progress;
+
+    public Profile(){
+        this.user = user;
+        this.stats = stats;
+        this.progress = progress;
+    }
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,31 @@
 public class User {
+    String userId;
+    String name;
+    String gender;
+    double weight;
+    double height;
+    int age;
+    String activityLevel;
+    double goal;
+    String currentPlan;
 
+    public User(){
+        this.userId = userId;
+        this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.age = age;
+        this.activityLevel = activityLevel;
+        this.goal = goal;
+        this.currentPlan = currentPlan;
+    }
+
+    public void createUser(){
+        
+    }
+
+    public void login(){
+
+    }
 }
